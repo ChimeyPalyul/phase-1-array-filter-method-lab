ac41feeeba0a20f04d3b8652529ddaca14c1c5af
Chimey Palyul <palyul.chimey@gmail.com>
2023-09-18 16:49:45 -0600

    Revist/ dont fully grasp syntax/method

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+const drivers = ["Bobby","Taylor","Mike","Jesus"]
+
+function findMatching(collection,name){
+    return collection.filter(
+     (possibleMatch) => possibleMatch.toLowerCase() === name.toLowerCase()
+    )
+}
+
+
+
+
+
+
+
+
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
